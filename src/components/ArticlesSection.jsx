19bfe30e3cbf19676ebb1e2b474f16a0d9cd0072
Sevinj0124 <Cavadova.24@icloud.com>
2024-05-03 20:34:42 +0400

    pecode

--- src/components/ArticlesSection.jsx
+++ src/components/ArticlesSection.jsx
@@ -4,40 +4,42 @@ import Card from './Card'
 const ArticlesSection = () => {
   return (
     <div>
-<div className="articles-two">
+<div className="articles-two container">
    <div className="row">
     <div className="col-12 col-md-6">
-      <Card kartinShekli="/assets/images/blog1.jpeg" kartinBasligi = "Yeni xeber" kartinParaqrafi = " burada sizin"/>
+      <Card kartinShekli="/assets/images/blog1.jpeg" kartinBasligi = "The truth about ReactJS" kartinParaqrafi = " Sigh, it’s like we can’t catch a break. So much has happened over the past happened happened over the past happened....."/>
     </div>
     <div className="col-12 col-md-6">
-      <Card kartinShekli="/assets/images/blog2.png" kartinBasligi = "Yeni xeber 2" kartinParaqrafi = " burada sizin"/>
+      <Card kartinShekli="/assets/images/blog2.png" kartinBasligi = "How to solve an unsolvable
+conflict " kartinParaqrafi = "Sigh, it’s like we can’t catch a break. So much has happened over the past happened happened over the past happened....."/>
     </div>
     
    </div>
 </div>
 
-<div className="articles-three">
-</div>
+<div className="articles-three container">
 <div className="row">
 <div className="col-12 col-md-6 col-lg-4">
-  <Card />
+  <Card kartinShekli="/assets/images/blog3.png" kartinBasligi = "The truth about ReactJS" kartinParaqrafi = " Sigh, it’s like we can’t catch a break. So much has happened over the past......."/> 
 </div>
 <div className="col-12 col-md-6 col-lg-4">
-  <Card />
+  <Card kartinShekli="/assets/images/blog4.png" kartinBasligi = "Gaffer. Case Study" kartinParaqrafi = " Sigh, it’s like we can’t catch a break. So much has happened over the past......."/> 
 </div>
 <div className="col-12 col-md-6 col-lg-4">
-  <Card />
+  <Card kartinShekli="/assets/images/blog5.png" kartinBasligi = "How to solve an unsolvable
+conflict " kartinParaqrafi = "Sigh, it’s like we can’t catch a break. So m....."/> 
 </div>
 <div className="col-12 col-md-6 col-lg-4">
-  <Card />
+  <Card kartinShekli="/assets/images/blog6.png" kartinBasligi = "The truth about ReactJS" kartinParaqrafi = "Sigh, it’s like we can’t catch a break. So much has happened over the past......."/> 
 </div>
 <div className="col-12 col-md-6 col-lg-4">
-  <Card />
+  <Card kartinShekli="/assets/images/blog7.png" kartinBasligi = "Gaffer. Case Study" kartinParaqrafi = " Sigh, it’s like we can’t catch a break. So much has happened over the past......."/> 
 </div>
 <div className="col-12 col-md-6 col-lg-4">
-  <Card />
+  <Card  kartinShekli="/assets/images/blog8.png" kartinBasligi = "How to solve an unsolvable" kartinParaqrafi = " Sigh, it’s like we can’t catch a break. So m...."/>
 </div>
 </div>
+    </div>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,14 @@ const Card = ({kartinShekli,kartinBasligi,kartinParaqrafi}) => {
   return (
     <div className='card border-0'>
 <img src={kartinShekli}alt="" className='blog-img' />
-<div className="card-body">
-  <h1>{kartinBasligi}</h1>
+<div className="card-body p-0 py-3">
+  <h1 className='card-title'>{kartinBasligi}</h1>
   <p>{kartinParaqrafi}</p>
+
+  <div className="metadata">
+  <img src="/assets/icons/favicon.svg" alt="" />
+  <span className='moderator'>Pecode Team  <span id="tarix">22.03.2024</span></span>
+</div>
 </div>
     </div>
   )
