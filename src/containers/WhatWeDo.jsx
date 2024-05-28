798d3a35846ee9c43f5d2c0c6e172cc10d6f2d4f
Sevinj0124 <Cavadova.24@icloud.com>
2024-05-29 01:40:39 +0400

    pecode

--- src/containers/WhatWeDo.jsx
+++ src/containers/WhatWeDo.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import './WhatWeDo.css'
 
 const WhatWeDo = () => {
   return (
-    <div>s4 pecode</div>
+    <> 
+    <div className='text-center container py-4'> 
+    <button className='how-it-works p-2'>How it works</button>
+    <h1 className='py-3'>It’s easy as 1, 2, 3</h1>
+    <p className='text-secondary py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, suspendisse varius enim in <br /> eros. Elementum tristique duis cursus, mi quis viverra ornare eros dolor.</p>
+        </div>
+
+{/* 
+<div className="row"> 
+   <div className="story-icons col-12 col-md-4 col-lg-4 d-flex ">
+      <div className="story-icons-1 ">
+      <img src="/assets/story/story1.svg" alt="" />
+<h4>Apply the product</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in  eros elementum tristique.</p>
+      </div>
+      <div className="story-icons-2">
+      <img src="/assets/story/story2.svg" alt="" />
+      <h4>Wait for the skin to absorb</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in  eros elementum tristique.</p>
+    
+      </div>
+      <div className="story-icons-3">
+         <img src="/assets/story/story3.svg" alt="" />
+<h4>And that’s it</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in  eros elementum tristique.</p>
+      </div>
+      </div>
+    </div> */}
+
+
+    </>
+
   )
 }
 
