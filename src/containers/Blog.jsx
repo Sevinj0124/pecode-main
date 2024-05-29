8d5c09b60eb26353953616b1ed237532b9f6614c
Sevinj0124 <Cavadova.24@icloud.com>
2024-05-29 14:04:19 +0400

    pecode

--- src/containers/Blog.jsx
+++ src/containers/Blog.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div className='text-center'>Blog sehifesi</div>
+    <div className="container py-5">
+  <div className="row">
+    <div className=" skin-left-sidecol-12 col-md-6">
+<img src="/assets/images/girl.svg" alt=""  className='w-100'/>
+    </div>
+    <div className=" skin-right-side col-12 col-md-6">
+<h6>Smooth skin collection</h6>
+<h2>For oily face skin</h2>
+<p className='text-secondary'>Lorem ipsum dolor sit amet, consectetur adipiscing elit suspendisse Varius enim in eros elementum tristique, duis cursus mi quis viverra ornare, eros dolor interdum nulla ut commodo.</p>
+   <div className="shop-now ">
+    <button className='shop p-2'>Shop now</button>
+    <button className='explore p-2'>Explore more <img src="/assets/icons/sagox.svg" alt="" /></button>
+   </div>
+    </div>
+  </div>
+    </div>
   )
 }
 
--- src/containers/Career.jsx
+++ src/containers/Career.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
 
 const Career = () => {
   return (
-    <div className='text-center'>Kariyera</div>
+    <div className="row">
+
+    
+    <div className=' col-12  p-5'>
+ <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+       autoplay = {
+        {
+          delay:2500,
+          disableOnInteraction: false,
+        }
+       }
+       
+        className="mySwiper"
+      >
+        <SwiperSlide><img src="/assets/images/swiper1.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper2.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper3.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper4.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper5.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper1.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper2.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper3.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper4.svg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="/assets/images/swiper5.svg" alt="" /></SwiperSlide>
+       
+ 
+      </Swiper>
+
+    </div>
+    </div>
   )
 }
 
